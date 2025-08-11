113b8d2c84a2765e7e504a6b18aed34fc8f044be
Fernando-Roque <fernando.mauricio.roque@gmail.com>
2025-08-11 11:41:37 -0300

    Adicionei o  interaçao com o usuário

--- src/App.java
+++ src/App.java
@@ -5,11 +5,15 @@
 // import service.CatalogoFilmes;
 
 import java.time.LocalDate;
-import java.util.Arrays;
+import java.util.List;
+// import java.util.Arrays;
+import java.util.Scanner;
 
 public class App {
         public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in); // Scanner para entrada de dados do usuário
+         
         CatalogoFilmes catalogo = new CatalogoFilmes(); // Instanciando o catálogo de filmes 
         
         // Criando alguns diretores 
@@ -17,11 +21,13 @@ public class App {
         Diretor scorsese = new Diretor("Martin Scorsese", LocalDate.of(1942, 11, 17), "Americano", 15);
         Diretor inarritu = new Diretor("Alejandro González Iñárritu", LocalDate.of(1963, 8, 15), "Mexicano", 12);
         Diretor villeneuve = new Diretor("Denis Villeneuve", LocalDate.of(1967, 10, 3), "Canadense", 8);
-        
+        Diretor stahelski = new Diretor("Chad Stahelski", LocalDate.of(1968, 9, 20), "Americano", 5);
+
         // Criando alguns atores
         Ator ator = new Ator("Leonardo DiCaprio", LocalDate.of(1974, 11, 11), "Americano", 3);
         Ator murphy = new Ator("Cillian Murphy", LocalDate.of(1976, 5, 25), "Irlandês", 2);
         Ator chalamet = new Ator("Timothée Chalamet", LocalDate.of(1995, 12, 27), "Americano", 1);
+        Ator reeves = new Ator("Keanu Reeves", LocalDate.of(1964, 9, 2), "Canadense", 4);
 
         // Adicionando um filme
         Filme filme = new Filme("A Origem", LocalDate.of(2010, 7, 16), 160_000_000, "Filme de ficção científica e ação.");
@@ -34,17 +40,19 @@ public class App {
         // Adicionando mais dois filmes do mesmo ator (Leonardo DiCaprio)        
         Filme loboWallStreet = new Filme("O Lobo de Wall Street", LocalDate.of(2013, 12, 25), 100_000_000, "Comédia negra biográfica.");
         Filme regresso = new Filme("O Regresso", LocalDate.of(2015, 12, 25), 135_000_000, "Drama de sobrevivência.");
-        
+        Filme johnWick1 = new Filme("John Wick", LocalDate.of(2014, 10, 24), 20_000_000, "Assassino aposentado busca vingança.");
         // Cadastrando diretores
         catalogo.cadastrarDiretor(diretor);
         catalogo.cadastrarDiretor(scorsese);
         catalogo.cadastrarDiretor(inarritu);
         catalogo.cadastrarDiretor(villeneuve);
+        catalogo.cadastrarDiretor(stahelski);
 
         // Cadastrando atores
         catalogo.cadastrarAtor(ator);
         catalogo.cadastrarAtor(murphy);
         catalogo.cadastrarAtor(chalamet);
+        catalogo.cadastrarAtor(reeves);
 
         // Cadastrando filmes
         catalogo.cadastrarFilme(filme);
@@ -54,6 +62,7 @@ public class App {
         catalogo.cadastrarFilme(regresso);
         catalogo.cadastrarFilme(oppenheimer);
         catalogo.cadastrarFilme(duna2);
+        catalogo.cadastrarFilme(johnWick1);
         
         // Associando diretores. 
         catalogo.associarDiretorFilme(filme, diretor);
@@ -61,6 +70,8 @@ public class App {
         catalogo.associarDiretorFilme(dunkirk, diretor);
         catalogo.associarDiretorFilme(loboWallStreet, scorsese);
         catalogo.associarDiretorFilme(regresso, inarritu);
+        catalogo.associarDiretorFilme(johnWick1, stahelski);
+        
 
         // Associando atores aos filmes 
         catalogo.associarAtorFilme(filme, ator);
@@ -68,15 +79,27 @@ public class App {
         catalogo.associarAtorFilme(regresso, ator);
         catalogo.associarAtorFilme(oppenheimer, murphy);
         catalogo.associarAtorFilme(duna2, chalamet);
+        catalogo.associarAtorFilme(johnWick1, reeves);
         
         // Associando diretores aos filmes
         catalogo.associarDiretorFilme(oppenheimer, diretor); // Nolan já existente
-        catalogo.associarDiretorFilme(duna2, villeneuve);      
+        catalogo.associarDiretorFilme(duna2, villeneuve); 
+        catalogo.associarDiretorFilme(johnWick1, stahelski); // Stahelski já existente     
     
         // Listando todos os filmes cadastrados
         System.out.println( catalogo.listarFilmes());
+        
+        System.out.print("Digite um Filme que esteja no Catalogo de Filmes a cima : ");
+        String palavra = scanner.nextLine(); // melhor que next() para aceitar espaços
 
-        System.out.println("Filmes encontrados: " + catalogo.pesquisarFilmePorNome("oppenheimer"));
+        List<Filme> resultados = catalogo.pesquisarFilmePorNome(palavra);
+
+        if (!resultados.isEmpty()) {
+            System.out.println("Filmes encontrados: " + resultados);
+        } else {
+            System.out.println("Nenhum filme encontrado com esse nome.");
+        }
+
     }
 }
 
